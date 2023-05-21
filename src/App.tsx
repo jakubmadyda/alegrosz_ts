f9c9e9fa6d0cc043370cf22635dbbe550985fe49
Jakub Madyda <jakub.madyda17@gmail.com>
2023-05-21 08:56:24 +0200

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-      <h1>Alegrosz</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Alegrosz</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
